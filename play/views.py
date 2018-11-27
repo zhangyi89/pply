ec496198dddc89c60e79e7ea3270dd424975d058
毅 张 <zhangyi_1@chinatelecom.cn>
2018-11-27 09:35:54 +0800

    修改播放域名

--- play/views.py
+++ play/views.py
@@ -9,6 +9,7 @@ logger = logging.getLogger("play_live")
 def play_live(request, *args, **kwargs):
     path = request.get_full_path()
     url = "http://183.57.148.19:8000"
+    url = "http://zb.ctzcdn.net"
     url = f"{url}/{path}/playlist.m3u8"
     logger.info(url)
     return render(request, "rtmpPlay.html", {"url": url})
